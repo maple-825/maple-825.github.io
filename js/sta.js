25cd65582c671acc4c0220be5723adbf5628d871
maple-825 <1602411529@qq.com>
2021-05-21 15:52:58 +0800

    sta

---
+++ js/sta.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f1dbe6bbb32bca13895f929d99ae18e1";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
